40c13e610b37e5bd2c86a16a24682d12c99dc398
kosne1 <akolia2017@list.ru>
2022-02-02 18:41:27 +0300

    feat: add StudentException

---
+++ Isu/Tools/SpecificExceptions/StudentException.cs
@@ -0,0 +1,19 @@
+﻿namespace Isu.Tools.SpecificExceptions
+{
+    public class StudentException : IsuException
+    {
+        public StudentException()
+        {
+        }
+
+        public StudentException(string message)
+            : base(message)
+        {
+        }
+
+        public StudentException(string message, IsuException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
