c9299e469532e80a47f18653aa3ca669e6a38d16
kosne1 <akolia2017@list.ru>
2022-02-12 13:35:34 +0300

    feat: add Student.cs, Group.cs

---
+++ IsuExtra/Entities/Group.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsuExtra.Entities
+{
+    public class Group
+    {
+        private List<Student> _students;
+        public Group(GroupName groupName)
+        {
+            Id = Guid.NewGuid();
+            GroupName = groupName;
+            _students = new List<Student>();
+        }
+
+        public Guid Id { get; }
+        public GroupName GroupName { get; }
+
+        public List<Student> StudentList()
+        {
+            return new List<Student>(_students);
+        }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public void Remove(Student student)
+        {
+            _students.Remove(student);
+        }
+    }
+}
---
+++ IsuExtra/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IsuExtra.Entities
+{
+    public class Student
+    {
+        public Student(string name, GroupName groupName)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Groupname = groupName;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public GroupName Groupname { get; }
+    }
+}
