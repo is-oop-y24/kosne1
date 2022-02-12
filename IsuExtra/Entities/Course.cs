29c8d677b46f13517b3552fad5a106be655aace6
kosne1 <akolia2017@list.ru>
2022-02-12 13:52:51 +0300

    feat: add Course.cs, Faculty.cs

---
+++ IsuExtra/Entities/Course.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace IsuExtra.Entities
+{
+    public class Course
+    {
+        private List<Group> _groups;
+
+        public Course(CourseNumber courseNumber)
+        {
+            CourseNumber = courseNumber;
+            _groups = new List<Group>();
+        }
+
+        public CourseNumber CourseNumber { get; }
+    }
+}
---
+++ IsuExtra/Entities/Faculty.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace IsuExtra.Entities
+{
+    public class Faculty
+    {
+        private List<Course> _courses;
+
+        public Faculty(char facultyName)
+        {
+            FacultyName = facultyName;
+            _courses = new List<Course>();
+            for (int i = 1; i < 5; i++)
+            {
+                _courses[i] = new Course((CourseNumber)i);
+            }
+        }
+
+        public char FacultyName { get; }
+    }
+}
