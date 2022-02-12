7f83a605cfabe0218512d68bd2e906ab465c93de
kosne1 <akolia2017@list.ru>
2022-02-13 01:13:00 +0300

    feat: add GroupException.cs

---
+++ IsuExtra/Tools/SpecificExceptions/GroupException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IsuExtra.Tools.SpecificExceptions
+{
+    public class GroupException : IsuExtraException
+    {
+        public GroupException()
+        {
+        }
+
+        public GroupException(string message)
+            : base(message)
+        {
+        }
+
+        public GroupException(string message, IsuExtraException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
