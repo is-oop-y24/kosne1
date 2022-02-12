d4f6a6d4045e2c3593084b4d4608eb21ef4650d8
kosne1 <akolia2017@list.ru>
2022-02-12 20:22:12 +0300

    feat: add CampusAddress.cs

---
+++ IsuExtra/Entities/UniversityFacilities/CampusAddress.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace IsuExtra.Entities.UniversityFacilities
+{
+    public enum CampusAddress
+    {
+        [Description("St. Petersburg, Kronverksky prospect, 49, lit. A.")]
+        Address1,
+        [Description("St. Petersburg, Birzhevaya line, 14, lit. A")]
+        Address2,
+        [Description("St. Petersburg, Grivtsova lane, 14-16, lit.A")]
+        Address3,
+        [Description("St. Petersburg, Lomonosova street, 9")]
+        Address4,
+        [Description("St. Petersburg, Tchaikovsky street, 11/2, lit.A")]
+        Address5,
+    }
+}
