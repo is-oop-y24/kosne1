6f335250481c5e8b1a4da26bc39fea1cb3cef90f
kosne1 <akolia2017@list.ru>
2022-02-13 00:15:41 +0300

    feat: add Teacher.cs

---
+++ IsuExtra/Entities/UniversityPeople/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IsuExtra.Entities.UniversityPeople
+{
+    public class Teacher
+    {
+        public Teacher(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+    }
+}
