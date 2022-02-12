bace854af63c064973ee14b2cb4c7054da2324f5
kosne1 <akolia2017@list.ru>
2022-02-13 01:12:20 +0300

    feat: add ListStudentException.cs

---
+++ IsuExtra/Tools/SpecificExceptions/ListStudentException.cs
@@ -0,0 +1,19 @@
+﻿namespace IsuExtra.Tools.SpecificExceptions
+{
+    public class ListStudentException : IsuExtraException
+    {
+        public ListStudentException()
+        {
+        }
+
+        public ListStudentException(string message)
+            : base(message)
+        {
+        }
+
+        public ListStudentException(string message, IsuExtraException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
