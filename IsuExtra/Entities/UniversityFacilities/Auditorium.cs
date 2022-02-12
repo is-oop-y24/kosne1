1e391a88a6271a915e6b56987e360737cb0eca99
kosne1 <akolia2017@list.ru>
2022-02-12 20:22:28 +0300

    feat: add Auditorium.cs

---
+++ IsuExtra/Entities/UniversityFacilities/Auditorium.cs
@@ -0,0 +1,18 @@
+﻿using IsuExtra.Services.Description;
+
+namespace IsuExtra.Entities.UniversityFacilities
+{
+    public class Auditorium
+    {
+        public Auditorium(int number, CampusAddress campusAddress)
+        {
+            Number = number;
+            DescriptionStrategy = new DescriptionStrategy();
+            Address = DescriptionStrategy.GetDescription(campusAddress);
+        }
+
+        public int Number { get; }
+        public string Address { get; }
+        public IDescriptionStrategy DescriptionStrategy { private get; set; }
+    }
+}
