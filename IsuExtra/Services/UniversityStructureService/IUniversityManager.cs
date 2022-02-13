be9643a2c4a0038e5dab3ab21ebc7ff13b593d5a
kosne1 <akolia2017@list.ru>
2022-02-13 22:04:01 +0300

    feat: add IUniversityManager.cs

---
+++ IsuExtra/Services/UniversityStructureService/IUniversityManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using IsuExtra.Entities.NamesOfUniversityStructures;
+using IsuExtra.Entities.UniversityPeople;
+using IsuExtra.Entities.UniversityStructure;
+
+namespace IsuExtra.Services.UniversityStructureService
+{
+    public interface IUniversityManager
+    {
+        Faculty AddFaculty(char facultyName);
+        Course AddCourse(Faculty faculty, CourseNumber courseNumber);
+        Group AddGroup(GroupName groupName);
+        Student AddStudent(Group group, string studentName);
+
+        Student FindStudent(Guid id);
+        Student FindStudent(string studentName);
+        List<Student> FindStudents(GroupName groupName);
+
+        Group FindGroup(GroupName groupName);
+
+        void ChangeStudentGroup(Student student, Group newGroup);
+    }
+}
