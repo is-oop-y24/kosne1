799ce99298b2f99d03b2556c88c6dd72fbd4ce67
kosne1 <akolia2017@list.ru>
2022-02-13 21:38:27 +0300

    feat: add FacultyException.cs.cs

---
+++ IsuExtra/Tools/SpecificExceptions/FacultyException.cs
@@ -0,0 +1,19 @@
+﻿namespace IsuExtra.Tools.SpecificExceptions
+{
+    public class FacultyException : IsuExtraException
+    {
+        public FacultyException()
+        {
+        }
+
+        public FacultyException(string message)
+            : base(message)
+        {
+        }
+
+        public FacultyException(string message, IsuExtraException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
