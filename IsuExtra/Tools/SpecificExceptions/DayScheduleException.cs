f8e68169c6ddb864690c99326b1dee6c9f864fd1
kosne1 <akolia2017@list.ru>
2022-02-14 11:47:31 +0300

    feat: add DayScheduleException.cs

---
+++ IsuExtra/Tools/SpecificExceptions/DayScheduleException.cs
@@ -0,0 +1,19 @@
+﻿namespace IsuExtra.Tools.SpecificExceptions
+{
+    public class DayScheduleException : IsuExtraException
+    {
+        public DayScheduleException()
+        {
+        }
+
+        public DayScheduleException(string message)
+            : base(message)
+        {
+        }
+
+        public DayScheduleException(string message, IsuExtraException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
