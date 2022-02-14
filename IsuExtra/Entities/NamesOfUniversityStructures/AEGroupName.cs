9c81b249b61241ff2e1eddf4b50a0b4bc151fde4
kosne1 <akolia2017@list.ru>
2022-02-14 15:40:30 +0300

    feat: add AEGroupName.cs and AEGroupNameException.cs

---
+++ IsuExtra/Entities/NamesOfUniversityStructures/AEGroupName.cs
@@ -0,0 +1,23 @@
+﻿using IsuExtra.Tools.SpecificExceptions;
+
+namespace IsuExtra.Entities.NamesOfUniversityStructures
+{
+    public class AEGroupName
+    {
+        public AEGroupName(string groupName)
+        {
+            if (groupName.Length != 5 || groupName[3] != '-'
+                                      || groupName[4] < '0'
+                                      || groupName[4] > '9')
+            {
+                throw new AEGroupNameException("Error: wrong additional education group format");
+            }
+
+            MegaFaculty = groupName[..3];
+            Number = groupName[4];
+        }
+
+        public string MegaFaculty { get; }
+        public int Number { get; }
+    }
+}
