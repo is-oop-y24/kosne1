29a002f4936e16e09803c597ef3862a20a5050e1
kosne1 <akolia2017@list.ru>
2022-02-14 14:21:33 +0300

    style: remove unnecessary using

--- IsuExtra/Services/DescriptionService/DescriptionStrategy.cs
+++ IsuExtra/Services/DescriptionService/DescriptionStrategy.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using IsuExtra.Entities.ScheduleStructure;
 
 namespace IsuExtra.Services.DescriptionService
 {
--- IsuExtra/Services/DescriptionService/IDescriptionStrategy.cs
+++ IsuExtra/Services/DescriptionService/IDescriptionStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using IsuExtra.Entities.ScheduleStructure;
 
 namespace IsuExtra.Services.DescriptionService
 {
