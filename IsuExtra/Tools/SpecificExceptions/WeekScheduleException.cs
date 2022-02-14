23706be4fac1cb4ff07ca7394f511a4ca07defb6
kosne1 <akolia2017@list.ru>
2022-02-14 14:20:49 +0300

    feat: add WeekScheduleException.cs

---
+++ IsuExtra/Tools/SpecificExceptions/WeekScheduleException.cs
@@ -0,0 +1,19 @@
+﻿namespace IsuExtra.Tools.SpecificExceptions
+{
+    public class WeekScheduleException : IsuExtraException
+    {
+        public WeekScheduleException()
+        {
+        }
+
+        public WeekScheduleException(string message)
+            : base(message)
+        {
+        }
+
+        public WeekScheduleException(string message, IsuExtraException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
