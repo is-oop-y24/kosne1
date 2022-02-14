41b3ca9e10b93590cef119ba52362dc0a51650e2
kosne1 <akolia2017@list.ru>
2022-02-14 12:56:00 +0300

    feat: add GroupName overload operator ==, !=

--- IsuExtra/Entities/NamesOfUniversityStructures/GroupName.cs
+++ IsuExtra/Entities/NamesOfUniversityStructures/GroupName.cs
@@ -1,4 +1,5 @@
-﻿using IsuExtra.Tools.SpecificExceptions;
+﻿using System;
+using IsuExtra.Tools.SpecificExceptions;
 
 namespace IsuExtra.Entities.NamesOfUniversityStructures
 {
@@ -25,5 +26,60 @@ namespace IsuExtra.Entities.NamesOfUniversityStructures
         public LevelOfEducation LevelOfEducation { get; }
         public CourseNumber CourseNumber { get; }
         public int GroupNumber { get; }
+
+        public static bool operator ==(GroupName groupName1, GroupName groupName2)
+        {
+            if (groupName1 == null)
+            {
+                throw new GroupNameException("Error: null groupName1 value");
+            }
+
+            if (groupName2 == null)
+            {
+                throw new GroupNameException("Error: null groupName2 value");
+            }
+
+            return (groupName1.Faculty == groupName2.Faculty) &&
+                   ((int)groupName1.LevelOfEducation == (int)groupName2.LevelOfEducation) &&
+                   ((int)groupName1.CourseNumber == (int)groupName2.CourseNumber) &&
+                   (groupName1.GroupNumber == groupName2.GroupNumber);
+        }
+
+        public static bool operator !=(GroupName groupName1, GroupName groupName2)
+        {
+            if (groupName1 == null)
+            {
+                throw new GroupNameException("Error: null groupName1 value");
+            }
+
+            if (groupName2 == null)
+            {
+                throw new GroupNameException("Error: null groupName2 value");
+            }
+
+            return (groupName1.Faculty != groupName2.Faculty) ||
+                   ((int)groupName1.LevelOfEducation != (int)groupName2.LevelOfEducation) ||
+                   ((int)groupName1.CourseNumber != (int)groupName2.CourseNumber) ||
+                   (groupName1.GroupNumber != groupName2.GroupNumber);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as GroupName);
+        }
+
+        public bool Equals(GroupName other)
+        {
+            return other != null &&
+                   Faculty == other.Faculty &&
+                   LevelOfEducation == other.LevelOfEducation &&
+                   CourseNumber == other.CourseNumber &&
+                   GroupNumber == other.GroupNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Faculty, LevelOfEducation, CourseNumber, GroupNumber);
+        }
     }
 }
