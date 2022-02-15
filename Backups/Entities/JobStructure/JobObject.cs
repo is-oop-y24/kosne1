2dfd1915ec89b381eccb260dbb174988f80ba2f8
kosne1 <akolia2017@list.ru>
2022-02-16 01:44:21 +0300

    feat: add JobObject.cs

---
+++ Backups/Entities/JobStructure/JobObject.cs
@@ -0,0 +1,12 @@
+﻿namespace Backups.Entities.JobStructure
+{
+    public class JobObject
+    {
+        public JobObject(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+    }
+}
