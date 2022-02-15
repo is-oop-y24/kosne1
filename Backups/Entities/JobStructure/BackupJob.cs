da28c9a0cbf675f66ea81860208deb6eac489785
kosne1 <akolia2017@list.ru>
2022-02-16 01:44:52 +0300

    feat: add BackupJob.csp

---
+++ Backups/Entities/JobStructure/BackupJob.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Backups.Entities.JobStructure
+{
+    public class BackupJob
+    {
+        private List<JobObject> jobObjects;
+        private List<RestorePoint> restorePoints;
+
+        public BackupJob()
+        {
+            jobObjects = new List<JobObject>();
+            restorePoints = new List<RestorePoint>();
+        }
+
+        public JobObject AddJobObject(JobObject jobObject)
+        {
+            jobObjects.Add(jobObject);
+            return jobObject;
+        }
+    }
+}
