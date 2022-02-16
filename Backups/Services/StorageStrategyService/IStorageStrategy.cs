a99187a177a05e071dabbf3911a9255014573250
kosne1 <akolia2017@list.ru>
2022-02-16 19:28:50 +0300

    feat: add StorageStrategy

---
+++ Backups/Services/StorageStrategyService/IStorageStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Backups.Entities.JobStructure;
+
+namespace Backups.Services.StorageStrategyService
+{
+    public interface IStorageStrategy
+    {
+        List<Storage> JobObjectsToStorages(List<JobObject> jobObjects);
+    }
+}
---
+++ Backups/Services/StorageStrategyService/SingleStorageStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Backups.Entities.JobStructure;
+
+namespace Backups.Services.StorageStrategyService
+{
+    public class SingleStorageStrategy : IStorageStrategy
+    {
+        public List<Storage> JobObjectsToStorages(List<JobObject> jobObjects)
+        {
+            return new List<Storage>() { new Storage(jobObjects) };
+        }
+    }
+}
