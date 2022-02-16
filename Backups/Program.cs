3706a2129e613e1d108445049763551aa18d843a
kosne1 <akolia2017@list.ru>
2022-02-16 23:30:17 +0300

    feat: add program backup check

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Backups
+﻿using System.Collections.Generic;
+using Backups.Entities.JobStructure;
+using Backups.Entities.Repository;
+using Backups.Services.StorageStrategyService;
+
+namespace Backups
 {
     internal class Program
     {
         private static void Main()
         {
+            IRepository repository = new RepositoryWithFileSystem(@"D:\Users\akoli\RiderProjects\kosne1\Backups\backups");
+            var backupJob = new BackupJob(repository);
+            var jobObjectA = new JobObject(@"D:\Users\akoli\RiderProjects\kosne1\Backups\TestDirectory\FileA.txt");
+            var jobObjectB = new JobObject(@"D:\Users\akoli\RiderProjects\kosne1\Backups\TestDirectory\FileB.txt");
+            var jobObjectC = new JobObject(@"D:\Users\akoli\RiderProjects\kosne1\Backups\TestDirectory\FileC.txt");
+            backupJob.AddJobObject(jobObjectA);
+            backupJob.AddJobObject(jobObjectB);
+            backupJob.AddJobObject(jobObjectC);
+            var jobObjects = new List<JobObject> { jobObjectA, jobObjectB, jobObjectC };
+            backupJob.StorageStrategy = new SingleStorageStrategy();
+            backupJob.Backup(jobObjects);
         }
     }
 }
