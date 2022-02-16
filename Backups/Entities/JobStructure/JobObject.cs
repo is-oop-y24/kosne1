f8dc52b1dcdabaffffed5bcba16f777731141522
kosne1 <akolia2017@list.ru>
2022-02-16 15:25:18 +0300

    feat: add Guid

--- Backups/Entities/JobStructure/JobObject.cs
+++ Backups/Entities/JobStructure/JobObject.cs
@@ -1,12 +1,16 @@
-﻿namespace Backups.Entities.JobStructure
+﻿using System;
+
+namespace Backups.Entities.JobStructure
 {
     public class JobObject
     {
         public JobObject(string path)
         {
             Path = path;
+            Id = Guid.NewGuid();
         }
 
         public string Path { get; }
+        public Guid Id { get; }
     }
 }
