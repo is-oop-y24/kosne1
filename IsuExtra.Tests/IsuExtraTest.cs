ddd8d88148975e8a412181b1136032b8772b0f4a
kosne1 <akolia2017@list.ru>
2022-02-19 20:04:38 +0300

    test: add AddStudentToAEGroup_StudentIsAlreadyIn2Groups_ThrowException

--- IsuExtra.Tests/IsuExtraTest.cs
+++ IsuExtra.Tests/IsuExtraTest.cs
@@ -289,5 +289,58 @@ namespace IsuExtra.Tests
                 aeUniversityManager.AddStudent(aeGroup, student);
             });
         }
+
+        [Test]
+        public void AddStudentToAEGroup_StudentIsAlreadyIn2Groups_ThrowException()
+        {
+            var groupName = new GroupName("M3207");
+            Faculty faculty = universityManager.AddFaculty(groupName.Faculty);
+            Course course = universityManager.AddCourse(faculty, groupName.CourseNumber);
+            Group group = universityManager.AddGroup(groupName);
+            
+            var teacher1 = new Teacher("Шоев Владислав Иванович");
+            var auditorium1 = new Auditorium(545, CampusAddress.Address2);
+
+            var teacher2 = new Teacher("Полозков Роман Григорьевич");
+            var auditorium2 = new Auditorium(550, CampusAddress.Address2);
+            
+            scheduleManager.AddGroupSchedule(groupName);
+            scheduleManager.AddDaySchedule(DayOfWeek.Monday, groupName);
+            Lesson lesson1 = scheduleManager.AddLesson(DayOfWeek.Monday, LessonBeginning.Lesson3, teacher1, groupName, auditorium1);
+            Lesson lesson2 = scheduleManager.AddLesson(DayOfWeek.Monday, LessonBeginning.Lesson5, teacher2, groupName, auditorium2);
+            
+            AECourse aeCourse1 = aeUniversityManager.AddCourse("СИА", 'N');
+            var aeGroupName1 = new AEGroupName("СИА-3");
+            AEGroup aeGroup1 = aeUniversityManager.AddGroup(aeGroupName1);
+            
+            var teacher3 = new Teacher("Береснев Артем Дмитриевич");
+            var auditorium3 = new Auditorium(500, CampusAddress.Address2);
+            
+            scheduleManager.AddGroupSchedule(aeGroupName1);
+            DaySchedule day1 = scheduleManager.AddDaySchedule(DayOfWeek.Monday, aeGroupName1);
+            scheduleManager.AddLesson(DayOfWeek.Monday, LessonBeginning.Lesson4, teacher3, aeGroupName1, auditorium3);
+
+            AECourse aeCourse2 = aeUniversityManager.AddCourse("КИБ", 'P');
+            var aeGroupName2 = new AEGroupName("КИБ-2");
+            AEGroup aeGroup2 = aeUniversityManager.AddGroup(aeGroupName2);
+
+            var teacher4 = new Teacher("Татьяна Тимофеевна Исаева");
+            var auditorium4 = new Auditorium(499, CampusAddress.Address2);
+         
+            AECourse aeCourse3 = aeUniversityManager.AddCourse("КТУ", 'K');
+            var aeGroupName3 = new AEGroupName("КТУ-2");
+            AEGroup aeGroup3 = aeUniversityManager.AddGroup(aeGroupName3);
+            
+            Student student = universityManager.AddStudent(group, "Ананьин Николай");
+
+            universityManager.AddStudent(group, student.Name);
+            aeUniversityManager.AddStudent(aeGroup1, student);
+            aeUniversityManager.AddStudent(aeGroup2, student);
+            
+            Assert.Catch<StudentException>(() =>
+            {
+                aeUniversityManager.AddStudent(aeGroup3, student);
+            });
+        }
     }
 }
