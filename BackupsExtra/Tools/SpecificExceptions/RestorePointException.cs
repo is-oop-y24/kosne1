104f67401ca3ab0b6b503c2b92a41835276cf760
kosne1 <akolia2017@list.ru>
2022-03-08 15:40:42 +0300

    feat: add RestorePointException.cs

---
+++ BackupsExtra/Tools/SpecificExceptions/RestorePointException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BackupsExtra.Tools.SpecificExceptions
+{
+    public class RestorePointException : Exception
+    {
+        public RestorePointException()
+        {
+        }
+
+        public RestorePointException(string message)
+            : base(message)
+        {
+        }
+
+        public RestorePointException(string message, BackupsException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
