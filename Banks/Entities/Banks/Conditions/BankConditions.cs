e4cffb51e4882a78893ffac154abb760369d7f7c
kosne1 <akolia2017@list.ru>
2022-03-21 23:00:50 +0300

    feat: add bank conditions

--- Banks/Entities/Banks/Conditions/BankConditions.cs
+++ Banks/Entities/Banks/Conditions/BankConditions.cs
@@ -1,22 +1,63 @@
-﻿namespace Banks.Entities.Banks.Conditions
+﻿using Banks.Tools.ObserverPattern;
+
+namespace Banks.Entities.Banks.Conditions
 {
-    public class BankConditions
+    public class BankConditions : EventCreator<Client>
     {
+        private UnconfirmedCondition unconfirmedCondition;
+        private DebitCondition debitCondition;
+        private DepositCondition depositCondition;
+        private CreditCondition creditCondition;
         public BankConditions(
             UnconfirmedCondition unconfirmedCondition,
             DebitCondition debitCondition,
             DepositCondition depositCondition,
             CreditCondition creditCondition)
         {
-            UnconfirmedCondition = unconfirmedCondition;
-            DebitCondition = debitCondition;
-            DepositCondition = depositCondition;
-            CreditCondition = creditCondition;
+            this.unconfirmedCondition = unconfirmedCondition;
+            this.debitCondition = debitCondition;
+            this.depositCondition = depositCondition;
+            this.creditCondition = creditCondition;
         }
 
-        public UnconfirmedCondition UnconfirmedCondition { get; }
-        public DebitCondition DebitCondition { get; }
-        public DepositCondition DepositCondition { get; }
-        public CreditCondition CreditCondition { get; }
+        public UnconfirmedCondition UnconfirmedCondition
+        {
+            get => unconfirmedCondition;
+            set
+            {
+                unconfirmedCondition = value;
+                Inform(UnconfirmedCondition.ConditionName);
+            }
+        }
+
+        public DebitCondition DebitCondition
+        {
+            get => debitCondition;
+            set
+            {
+                debitCondition = value;
+                Inform(DebitCondition.ConditionName);
+            }
+        }
+
+        public DepositCondition DepositCondition
+        {
+            get => depositCondition;
+            set
+            {
+                depositCondition = value;
+                Inform(DepositCondition.ConditionName);
+            }
+        }
+
+        public CreditCondition CreditCondition
+        {
+            get => creditCondition;
+            set
+            {
+                creditCondition = value;
+                Inform(CreditCondition.ConditionName);
+            }
+        }
     }
 }
