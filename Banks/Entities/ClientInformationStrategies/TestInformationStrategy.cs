ec62be663cbd4af73d5f4fd5d5c6bc6ad3355300
kosne1 <akolia2017@list.ru>
2022-03-21 23:37:20 +0300

    feat: add TestInformationStrategy.cs

---
+++ Banks/Entities/ClientInformationStrategies/TestInformationStrategy.cs
@@ -0,0 +1,11 @@
+﻿namespace Banks.Entities.ClientInformationStrategies
+{
+    public class TestInformationStrategy : IInformationStrategy
+    {
+        public string LastNotification { get; set; }
+        public void Inform(string contactName, string eventText)
+        {
+            LastNotification = $"{contactName}";
+        }
+    }
+}
